df7d7186afbdb34f2b27308816776e23fb1b81ef
sbmaruf <sbmaruf@gmail.com>
2017-11-23 10:58:59 +0800

    added run script

---
+++ Code/Phase_4/run.sh
@@ -0,0 +1,2 @@
+ python setup.py --train './../../Data/esp.train' --vocab './../../Data/esp.train ./../../Data/esp.testa ./../../Data/esp.testb' --dev './../../Data/esp.testa' --test './../../Data/esp.testb' --init_emb './../../Data/ES64' --embed_dim 64
+
